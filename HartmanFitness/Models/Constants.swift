2b834c7c27e315345eeb19a70d50970dc83f4f5b
Christopher Simonson <simonsonchristopher@gmail.com>
2021-03-14 20:41:47 -0700

    Added different color Assets

---
+++ HartmanFitness/Models/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  HartmanFitness
+//
+//  Created by Christopher Simonson on 3/14/21.
+//
+
+import Foundation
+import UIKit
+
+// Any constant values can be listed here, separated by inner enums for simplification and readability
+enum Constants {
+    /* Example
+    enum General {
+        public static let strokeWidth = CGFloat(2.0)
+    }*/
+}
---
+++ HartmanFitness/Views/TextViews.swift
@@ -0,0 +1,25 @@
+//
+//  TextViews.swift
+//  HartmanFitness
+//
+//  Created by Christopher Simonson on 3/14/21.
+//
+
+import SwiftUI
+
+/// Handles all the different types of text properties for the app.
+///
+/// Use structs to define different text styling that will be used in the app for repeatability.
+
+struct TextViews: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+/// Setup the struct previews here.
+struct TextViews_Previews: PreviewProvider {
+    static var previews: some View {
+        TextViews()
+    }
+}
