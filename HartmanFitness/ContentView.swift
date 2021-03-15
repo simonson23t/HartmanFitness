e93afec3e031091b904b34945e0c2008d5ead2a7
Christopher Simonson <simonsonchristopher@gmail.com>
2021-03-14 18:07:28 -0700

    Initial Commit

---
+++ HartmanFitness/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HartmanFitness
+//
+//  Created by Christopher Simonson on 3/14/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HartmanFitness/HartmanFitnessApp.swift
@@ -0,0 +1,17 @@
+//
+//  HartmanFitnessApp.swift
+//  HartmanFitness
+//
+//  Created by Christopher Simonson on 3/14/21.
+//
+
+import SwiftUI
+
+@main
+struct HartmanFitnessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
